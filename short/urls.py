b5027545124737e76c59c279c72609a7797da904
greyfine <84501816+greyfine@users.noreply.github.com>
2021-05-20 15:16:17 +0530

    Add files via upload

---
+++ short/urls.py
@@ -0,0 +1,28 @@
+"""short URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from urlshortner.views import dashboard ,deleteurl, home, generate 
+
+urlpatterns = [
+    """ defining URL patters for urlshortner"""
+    path('admin/', admin.site.urls),
+    path('', dashboard, name="dashboard"),
+    path('dashboard/', dashboard, name="dashboard"),
+    path('generate/', generate, name="generate"),
+    path('deleteurl/', deleteurl, name="deleteurl"),
+    path('<str:query>/', home, name="home"),
+]
